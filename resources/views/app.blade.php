262cc1e85f5bedd0c526965e238961499261a844
qaisaralzikra <qaisaralzikrah@gmail.com>
2025-07-21 14:19:21 +0800

    delete asset

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -22,9 +22,6 @@
 </head>
 
 <body>
-    <p>
-        qaisar
-    </p>
     @inertia
     <script src="{{ asset('assets/plugins/global/plugins.bundle.js') }}"></script>
     <script src="{{ asset('assets/js/scripts.bundle.js') }}"></script>
