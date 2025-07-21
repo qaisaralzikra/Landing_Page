41ed527691f56868edd9f8bd3b7146a018bb1200
qaisaralzikra <qaisaralzikrah@gmail.com>
2025-07-21 14:35:55 +0800

    intall vite

---
+++ resources/js/Page/Main/Dashboard.vue
@@ -0,0 +1,16 @@
+
+<script setup>
+
+</script>
+
+<template>
+    <div>
+        <h1>Selamat Datang, Admin Portal 👋</h1>
+        <p>Website Pengelolaan Portal Aplikasi Umum Pemerintah</p>
+    </div>
+</template>
+
+
+<style lang="scss" scoped>
+
+</style>
