c4a9c45162b69a7bcf154aa3b0da442ab0baeae1
rantisi01 <tisirantisi@gmail.com>
2025-07-22 11:43:52 +0800

    update sidebar

--- resources/js/Pages/Main/Admin/Main_Dashboard.vue
+++ resources/js/Pages/Main/Admin/Main_Dashboard.vue
@@ -1,10 +1,13 @@
 <template>
+    <main-sidebar />
     <div class="lh-1 mt-10 ms-10">
         <h1 class=" fs-lg-2qx">Selamat Datang, Admin Portal 👋</h1>
         <p class=" fs-lg-4 fs-8" style="color: #3C3C43BF;">Website Pengelolaan Portal Aplikasi Umum Pemerintah</p>
     </div>
 </template>
 
-<script setup></script>
+<script setup>
+import MainSidebar from '../../../Components/Sidebar/Main-Sidebar.vue';
+</script>
 
 <style lang="scss" scoped></style>
